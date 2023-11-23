39f47a4693c1f677081160f7174bb5bd2789fa70
Tamires-Escobar <tamires.escobar@corebiz.ag>
2023-11-23 13:06:11 -0300

    main: config inital react app

---
+++ src/PAGES/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../Styles/styles_global.css'
+
+const Home_Page = () => {
+    return (
+        <div className="App">
+            <div className="App-render">Wellcome to home page </div>
+        </div>
+    );
+};
+
+export default Home_Page;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home_Page from './PAGES/Home/Home';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Home_Page />
+  </React.StrictMode>
+);
+
